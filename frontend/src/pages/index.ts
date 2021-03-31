e9b510dbbc1097002d1ac95f95f3a3bbee5f2521
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-31 09:24:02 -0300

    Exportação de todas as páginas

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Articles } from './Articles'
+export { default as Categories } from './Categories'
+export { default as Login } from './Login'
+export { default as Profile } from './Profile'
