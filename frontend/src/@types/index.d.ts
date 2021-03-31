9491275873ea5fd3b5159c8003d2c972d8fbb935
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-31 09:19:56 -0300

    Tipagem de interfaces

---
+++ frontend/src/@types/index.d.ts
@@ -0,0 +1,52 @@
+import "styled-components"
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    background: string
+    primary: string
+    title: string
+    shadow: string
+    text: string
+    profile: string
+    profileList: string
+    inputBg: string
+  }
+}
+
+export interface IUser {
+  id: number
+  name: string
+  nickname: string
+  email: string
+  avatar?: string
+  role: string
+  deletedAt: Date | null
+}
+
+export interface IComments {
+  id: number
+  userId?: number
+  name?: string
+  content: string
+  publishedIn: string
+  user?: IUser
+}
+
+export interface IArticles {
+  id: number
+  authorId: number
+  title: string
+  description: string
+  slug: string
+  image: string
+  publishedIn: string
+  author: IUser
+  comment: IComments[]
+  category: ICategories[]
+}
+
+export interface ICategories {
+  id: number
+  name: string
+  article: IArticles[]
+}
