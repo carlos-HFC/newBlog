aec8190d73a60ca342e5abc6b3dc0516ded70e3d
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-31 09:21:04 -0300

    Estrutura básica

---
+++ frontend/src/components/Page.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+
+interface IPage {
+  title: string
+}
+
+const Page: FC<IPage> = ({ children, title }) => {
+  return (
+    <main className="m-3">
+      <div className="container p-3">
+        <h2 className="text-center font-weight-bold mb-3" title={title}>{title}</h2><hr />
+        <section className="mt-4">
+          {children}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default Page
