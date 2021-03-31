0b12e6685d0c3c7d3ccce9816dddcdc033531d00
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-31 18:20:05 -0300

    Verificar se token não expirou, emitir uma mensagem ao usuário caso o token expire e efetuar logout

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,21 +1,23 @@
+import { isAfter } from 'date-fns'
 import { useContext, useEffect, useState } from 'react'
 import { Redirect, Route, RouteProps, Switch, useRouteMatch } from 'react-router-dom'
 import { DefaultTheme, ThemeProvider } from 'styled-components'
 import Cookies from 'js-cookie'
+import Swal from 'sweetalert2'
 
 import { IUser } from './@types'
 import { Header } from './components'
-import { UserProvider } from './context/UserContext'
 import { BlogContext } from './context/BlogContext'
+import { UserProvider } from './context/UserContext'
 import { Articles, Categories, Login, Profile } from './pages'
-import { isAuth } from './services/auth'
+import { isAuth, logout } from './services/auth'
 import api from './services/api'
 
 import { dark, light } from './css/themes'
 import GlobalStyle from './css/styles'
 
 function Private(props: RouteProps) {
-  if (!isAuth() && !Cookies.getJSON("user")) return <Redirect to={{ pathname: '/', state: props.location }} />
+  if (!isAuth()) return <Redirect to={{ pathname: '/', state: props.location }} />
 
   return (
     <>
@@ -24,11 +26,17 @@ function Private(props: RouteProps) {
   )
 }
 
+let countdown: NodeJS.Timeout
+
 function Routes() {
+  const route = useRouteMatch('/login')
   const { handleArticles, handleCategories, categories } = useContext(BlogContext)
 
+  const [time, setTime] = useState<Date | null>(null)
   const [theme, setTheme] = useState<DefaultTheme>(Cookies.getJSON("THEME_BLOG") || light)
   const [user, setUser] = useState<IUser>(Cookies.getJSON("user"))
+
+  const expiresToken = Cookies.get("EXP_TOKEN_BLOG")
 
   useEffect(() => {
     Promise.all([
@@ -38,19 +46,44 @@ function Routes() {
       handleCategories(response[0].data)
       handleArticles(response[1].data)
     })
-  }, [])
-
-  useEffect(() => {
     setUser(Cookies.getJSON("user"))
   }, [])
+
+  const cookieUser = Cookies.get("user")
+
+  useEffect(() => {
+    if (cookieUser !== "undefined") {
+      countdown = setTimeout(() => {
+        setTime(new Date())
+      }, 1000)
+    } else {
+      clearTimeout(countdown)
+      setTime(null)
+    }
+  }, [time, cookieUser])
 
   useEffect(() => {
     Cookies.set("THEME_BLOG", theme)
   }, [theme])
 
-  const handleTheme = () => setTheme(theme.title === 'light' ? dark : light)
+  if (isAfter(time as Date, new Date(String(expiresToken)))) {
+    clearTimeout(countdown)
+    setTime(null)
+    Swal.fire({
+      title: "Oopss...",
+      text: "Sua sessão expirou, você será redirecionado!!",
+      icon: "warning",
+      showConfirmButton: false,
+      timer: 5000,
+      didOpen: () => {
+        setTimeout(() => {
+          return logout()
+        }, 500)
+      }
+    })
+  }
 
-  const route = useRouteMatch('/login')
+  const handleTheme = () => setTheme(theme.title === 'light' ? dark : light)
 
   return (
     <Switch>
