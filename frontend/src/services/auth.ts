e17a8f8b061bbbdd1182dc70e410b342486bccc9
Carlos Faustino <chfcchfc96@gmail.com>
2021-03-31 09:24:37 -0300

    Funções para buscar informações delicadas

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,11 @@
+import Cookies from 'js-cookie'
+
+export const isAuth = () => Cookies.get("TOKEN_BLOG") !== undefined
+export const logout = () => {
+  Cookies.remove("TOKEN_BLOG")
+  Cookies.remove("EXP_TOKEN_BLOG")
+  Cookies.remove("user")
+  window.location.href = "/"
+}
+export const setToken = (token: string) => Cookies.set("TOKEN_BLOG", token)
+export const tokenExpires = (exp: number) => Cookies.set("EXP_TOKEN_BLOG", String(exp))
