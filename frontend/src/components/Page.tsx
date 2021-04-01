9ad6ee5609da9215b92362082f87e971fabae47f
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-01 18:06:20 -0300

    Tìtulo é opcional

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react"
 
 interface IPage {
-  title: string
+  title?: string
 }
 
 const Page: FC<IPage> = ({ children, title }) => {
   return (
     <main className="m-3">
       <div className="container p-3">
-        <h2 className="text-center font-weight-bold mb-3" title={title}>{title}</h2><hr />
+        {title && <><h2 className="text-center font-weight-bold mb-3" title={title}>{title}</h2><hr /></>}
         <section className="mt-4">
           {children}
         </section>
