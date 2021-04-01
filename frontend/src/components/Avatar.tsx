93173c47915a1f2cebbdf260eeaad7628b930f6d
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-01 18:04:42 -0300

    Tamanho do avatar configurável

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -5,14 +5,15 @@ import { avatars } from '../utils'
 
 interface IAvatar {
   avatar?: string
+  size?: number
 }
 
-const Avatar: FC<IAvatar> = ({ avatar }) => {
+const Avatar: FC<IAvatar> = ({ avatar, size = 150 }) => {
   const icon = avatars.find(el => el.name === avatar) as any
 
   return avatar
-    ? icon({ size: 150 })
-    : <BsPersonSquare size={150} />
+    ? icon({ size })
+    : <BsPersonSquare size={size} />
 }
 
 export default Avatar
