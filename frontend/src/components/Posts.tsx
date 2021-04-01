2d2cae0de8b8659515d6bc0623f565d1e28d77f8
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-01 18:06:50 -0300

    Mostrar o artigo selecionado e registrar o acesso

--- frontend/src/components/Posts.tsx
+++ frontend/src/components/Posts.tsx
@@ -1,26 +1,42 @@
 import { format, parseISO } from "date-fns"
 import { ptBR } from "date-fns/locale"
-import { FC } from "react"
+import { FC, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 import { IArticles } from "../@types"
+import api from "../services/api"
 
 interface IPost {
-  article: IArticles
+  articles: IArticles[]
 }
 
-const Posts: FC<IPost> = ({ article }) => {
+const Posts: FC<IPost> = ({ articles }) => {
+  const params = useParams() as { id: string }
+
+  useEffect(() => {
+    api.patch(`/articles/access/${params.id}`)
+  }, [])
+
   return (
-    <article className="blog__post">
-      <h2 className="blog__post-title">
-        {article.title}
-      </h2>
-      <p className="blog__post-meta">
-        {format(parseISO(article.publishedIn), "dd 'de' MMM 'de' yyyy", { locale: ptBR })} por {article.author.name}
-      </p>
-      <p>
-        {article.description}
-      </p>
-    </article>
+    <>
+      {articles?.map(article => article.id === Number(params.id) && (
+        <article className="article">
+          {article.image &&
+            <div className="article__img">
+              <img src={article.image} alt="Imagem do artigo" />
+            </div>
+          }
+          <h2>{article.title}</h2>
+          <ul>
+            {article.category.map(item => <li key={item.id} className="badge">{item.name}</li>)}
+          </ul>
+          <p className="article__meta">
+            {format(parseISO(String(article.publishedIn)), "dd 'de' MMM 'de' yyyy", { locale: ptBR })} por {article.author.name}
+          </p>
+          <p dangerouslySetInnerHTML={{ __html: String(article.description) }} />
+        </article>
+      ))}
+    </>
   )
 }
 
