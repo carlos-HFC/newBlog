8f2993afdaab40e031ef6a6c66bec8f1084dc2fb
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-06 18:32:37 -0300

    Mudança no box-shadow com modo claro

--- frontend/src/css/themes/index.ts
+++ frontend/src/css/themes/index.ts
@@ -13,7 +13,7 @@ export const light = {
   background: "#f1f5f7",
   primary: "#7950f2",
   title: "light",
-  shadow: "rgba(0,0,0,.3)",
+  shadow: "rgba(0,0,0,.5)",
   text: "#212529",
   profile: "#e9e9e9",
   profileList: "#d5d5d5",
