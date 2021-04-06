770208c61c598b63d70221cb85a97b4691aff58c
Carlos Faustino <chfcchfc96@gmail.com>
2021-04-06 18:01:55 -0300

    Associação de categoria e de artigos por conta do filtro

--- backend/src/categoryArticle/categoryArticle.model.ts
+++ backend/src/categoryArticle/categoryArticle.model.ts
@@ -1,4 +1,4 @@
-import { Column, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { BelongsTo, Column, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
 
 import { Article } from "src/article/article.model";
 import { Category } from "src/category/category.model";
@@ -14,6 +14,11 @@ export class CategoryArticle extends Model<CategoryArticle> {
     onDelete: 'CASCADE'
   })
   categoryId: number
+
+  @BelongsTo(() => Category)
+  category: Category[]
+  @BelongsTo(() => Article)
+  article: Article[]
 
   @ForeignKey(() => Article)
   @Column({
